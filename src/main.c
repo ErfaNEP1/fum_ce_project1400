492639dd0fc7ac206a5f94103badc8ff16eb8970
Narges Jafari <njafari14782@gmail.com>
2021-12-20 08:47:05 -0800

    add game of Word & board

--- src/main.c
+++ src/main.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
 #include "Models/Genome.h"
 #include "Models/Animal.h"
-#include "Models/Cell.h"
 #include "Models/Food.h"
+#include "Models/Cell.h"
 #include "Models/World.h"
 
+#define boardsize 20
 
 int main(){
-    Animal animal;
-    animal.healthPoint = 5;
 
-    printf("%d ", animal.healthPoint);
+    World game;
+    scanf("%d",game.x);
+    game.y = game.x;
+    char board[boardsize][boardsize];
+    for (int i = 0;i < game.x;i ++) {
+        for (int j = 0;j < game.y;j ++) {
+            board[i][j] = '.';
+        }
+    }
+    return 0;
 }
