f6e079cddec18bc65b13dd9404e72bd6608b26e0
elahefani <elahefani5@gmail.com>
2022-02-01 23:18:51 +0330

    added comment for attack function

--- src/AnimalMove/EnemyFunctions/Actions/Attack.c
+++ src/AnimalMove/EnemyFunctions/Actions/Attack.c
@@ -1,4 +1,21 @@
-void attack(Animal *attacker, Animal *enemy, Animal attackerPosition[], Animal enemyPosition[], int *attacherCount, int attackerposition, int *enemyCount, int enemyposition, int worldSize, Cell board[][worldSize])
+/**
+ * attacking given two animals
+ * 
+ * @param {Animal-attacker} attacker
+ * @param {Animal-enemy} enemy
+ * @param {Animal-attackerPosition[]} array of attacker animals 
+ * @param {Animal-enemyPosition[]} array of enemy animals 
+ * @param {int-attackerCount} number of attacker animals
+ * @param {int-attacherposition} position of attacher in array
+ * @param {int-enemyCount} number of enemy animals
+ * @param {int-enemyposition} position of enemy in array
+ * @param {int-worldSize} size of map
+ * @param {Cell-board[]} world map
+ * 
+ * @return {void}
+ */
+
+void attack(Animal *attacker, Animal *enemy, Animal attackerPosition[], Animal enemyPosition[], int *attackerCount, int attackerposition, int *enemyCount, int enemyposition, int worldSize, Cell board[][worldSize])
 {
     Cell defaultCell = {
         .typePlace = "default",
@@ -14,7 +31,7 @@ void attack(Animal *attacker, Animal *enemy, Animal attackerPosition[], Animal e
         else if (enemy->gene.attackPower > attacker->gene.defensePower)
         {
             enemy->energyPoint += attacker->energyPoint;
-            delete_animal(attackerposition, attackerPosition, &attacherCount);
+            delete_animal(attackerposition, attackerPosition, &attackerCount);
             board[attacker->x][attacker->y] = defaultCell;
         }
     }
